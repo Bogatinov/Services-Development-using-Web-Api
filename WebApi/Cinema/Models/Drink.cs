f3cc96c6007c6014b48f6d4ddc4fc6b11a145566
Aleksandar Bogatinov <aleksandar.bogatinov@haselt.com>
2015-07-16 20:50:43 +0200

    Add latest version from G1

---
+++ WebApi/Cinema/Models/Drink.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Models
+{
+    [Table("Drinks")]
+    public class Drink
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Liters { get; set; }
+    }
+}
