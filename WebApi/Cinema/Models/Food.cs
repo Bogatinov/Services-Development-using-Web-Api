703502b167b5d04ef917f3974018b50e05fac472
Aleksandar Bogatinov <aleksandar.bogatinov@haselt.com>
2015-07-16 20:50:43 +0200

    Add latest version from G1

---
+++ WebApi/Cinema/Models/Food.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Models
+{
+    [Table("Foods")]
+    public class Food
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ WebApi/Cinema/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Models
+{
+    [Table("Movies")]
+    public class Movie
+    {
+        public Movie()
+        {
+            Tickets = new HashSet<Ticket>();
+        }
+
+        [Key]
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public double Rating { get; set; }
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
