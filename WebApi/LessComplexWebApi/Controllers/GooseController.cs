2fe25ccb74d28ea38d3094a9825d7d36592e8d05
Aleksandar Bogatinov <aleksandar.bogatinov@haselt.com>
2015-07-16 18:39:36 +0200

    Exception handling with G1

---
+++ WebApi/LessComplexWebApi/Controllers/GooseController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LessComplexWebApi.Errors;
+
+namespace LessComplexWebApi.Controllers
+{
+    [RoutePrefix("api/v1/goose")]
+    public class GooseController : ApiController
+    {
+        // GET api/v1/goose/findGoose?name="Golden goose"
+        [HttpGet]
+        [NotFoundFilter]
+        [Route("findGoose", Name = "FindGoose")]
+        public IHttpActionResult FindGoose(string name)
+        {
+            GoldenGoose(name);
+            return Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateGoose(string name)
+        {
+            GoldenGoose(name);
+            return CreatedAtRoute("FindGoose", new {name = name}, name);
+        }
+
+        private int GoldenGoose(string name)
+        {
+            throw new NullReferenceException("Golden goose is not found");
+        }
+    }
+}
---
+++ WebApi/LessComplexWebApi/Errors/NotFoundFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace LessComplexWebApi.Errors
+{
+    public class NotFoundFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            if (context.Exception is ApplicationException)
+            {
+                Trace.WriteLine(context.Exception.Message);
+                context.Response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    RequestMessage = new HttpRequestMessage(context.Request.Method,
+                        context.Request.RequestUri),
+                    ReasonPhrase = context.Exception.Message
+                };
+            }
+        }
+    }
+}
