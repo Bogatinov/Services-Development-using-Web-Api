8ee362b20dc0086f05c94b19c3dd0feabffdb528
Aleksandar Bogatinov <aleksandar.bogatinov@haselt.com>
2015-07-17 17:26:39 +0200

    Fix bug with wrong exception log

--- WebApi/LessComplexWebApi/Errors/NotFoundFilterAttribute.cs
+++ WebApi/LessComplexWebApi/Errors/NotFoundFilterAttribute.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web;
 using System.Web.Http.Filters;
 
 namespace LessComplexWebApi.Errors
@@ -13,7 +10,7 @@ namespace LessComplexWebApi.Errors
     {
         public override void OnException(HttpActionExecutedContext context)
         {
-            if (context.Exception is ApplicationException)
+            if (context.Exception is NullReferenceException)
             {
                 Trace.WriteLine(context.Exception.Message);
                 context.Response = new HttpResponseMessage(HttpStatusCode.NotFound)
