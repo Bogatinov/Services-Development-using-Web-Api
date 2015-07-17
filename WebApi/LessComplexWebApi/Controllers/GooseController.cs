f06abc1e1f816832e04744ad5ef617272816cad2
Aleksandar Bogatinov <aleksandar.bogatinov@haselt.com>
2015-07-17 20:01:21 +0200

    Update to latest

--- WebApi/LessComplexWebApi/Controllers/GooseController.cs
+++ WebApi/LessComplexWebApi/Controllers/GooseController.cs
@@ -1,13 +1,4 @@
 ﻿using System;
-<<<<<<< HEAD
-using System.Diagnostics;
-=======
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
->>>>>>> Exception handling with G1
 using System.Web.Http;
 using LessComplexWebApi.Errors;
 
@@ -16,7 +7,6 @@ namespace LessComplexWebApi.Controllers
     [RoutePrefix("api/v1/goose")]
     public class GooseController : ApiController
     {
-<<<<<<< HEAD
         // GET api/v1/goose/find/myname
         [HttpGet]
         [Route("find/{name}")]
@@ -30,28 +20,6 @@ namespace LessComplexWebApi.Controllers
         public void FindGooldenGoose()
         {
             throw new NullReferenceException("This goose is not you are looking for.");
-=======
-        // GET api/v1/goose/findGoose?name="Golden goose"
-        [HttpGet]
-        [NotFoundFilter]
-        [Route("findGoose", Name = "FindGoose")]
-        public IHttpActionResult FindGoose(string name)
-        {
-            GoldenGoose(name);
-            return Ok();
-        }
-
-        [HttpPost]
-        public IHttpActionResult CreateGoose(string name)
-        {
-            GoldenGoose(name);
-            return CreatedAtRoute("FindGoose", new {name = name}, name);
-        }
-
-        private int GoldenGoose(string name)
-        {
-            throw new NullReferenceException("Golden goose is not found");
->>>>>>> Exception handling with G1
         }
     }
 }
--- WebApi/LessComplexWebApi/Errors/NotFoundFilterAttribute.cs
+++ WebApi/LessComplexWebApi/Errors/NotFoundFilterAttribute.cs
@@ -1,20 +1,6 @@
 ﻿using System;
-<<<<<<< HEAD
-<<<<<<< HEAD
 using System.Net;
 using System.Net.Http;
-=======
-using System.Collections.Generic;
-=======
->>>>>>> Fix bug with wrong exception log
-using System.Diagnostics;
-using System.Net;
-using System.Net.Http;
-<<<<<<< HEAD
-using System.Web;
->>>>>>> Exception handling with G1
-=======
->>>>>>> Fix bug with wrong exception log
 using System.Web.Http.Filters;
 
 namespace LessComplexWebApi.Errors
@@ -23,28 +9,14 @@ namespace LessComplexWebApi.Errors
     {
         public override void OnException(HttpActionExecutedContext context)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
             if (context.Exception is NullReferenceException) // what is your exception type
             {
                 context.Response = new HttpResponseMessage(HttpStatusCode.NotFound) // return 404
                 {
                     ReasonPhrase = context.Exception.Message, // return the message you passed in the exception
                     Content = new StringContent("This is a problem"), // return Content that this is a problem
-                    RequestMessage = new HttpRequestMessage(context.Request.Method, context.Request.RequestUri) // return I wanted GET api/v1/goose/find/myName
-=======
-            if (context.Exception is ApplicationException)
-=======
-            if (context.Exception is NullReferenceException)
->>>>>>> Fix bug with wrong exception log
-            {
-                Trace.WriteLine(context.Exception.Message);
-                context.Response = new HttpResponseMessage(HttpStatusCode.NotFound)
-                {
-                    RequestMessage = new HttpRequestMessage(context.Request.Method,
-                        context.Request.RequestUri),
-                    ReasonPhrase = context.Exception.Message
->>>>>>> Exception handling with G1
+                    RequestMessage = new HttpRequestMessage(context.Request.Method, context.Request.RequestUri)
+                    // return I wanted GET api/v1/goose/find/myName
                 };
             }
         }
