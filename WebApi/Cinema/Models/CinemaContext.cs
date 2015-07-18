b1dd587d55fcd2b7a0dd1640a9eec7e0362fb76a
Aleksandar Bogatinov <aleksandar.bogatinov@haselt.com>
2015-07-18 16:16:50 +0200

    Update to latest with G2

--- WebApi/Cinema/Models/CinemaContext.cs
+++ WebApi/Cinema/Models/CinemaContext.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -29,7 +31,35 @@ namespace Cinema.Models
         public async Task CreateMovie(Movie movie)
         {
             this.Movies.Add(movie);
-            await this.SaveChangesAsync();
+
+            await this.Save();
+        }
+
+        private async Task Save()
+        {
+            try
+            {
+                await this.SaveChangesAsync();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+
+                throw new DbEntityValidationException(
+                    "Entity Validation Failed - errors follow:\n" +
+                    sb.ToString(), ex
+                ); // Add the original exception as the innerException
+            }
         }
     }
 }
